19d8b8bc8f20bffbbf97865550a4b03e02e3644c
mennemann <marcinmennemann@gmail.com>
2022-11-16 14:55:26 +0100

    Added Fileout for render

--- src/output/display.cpp
+++ src/output/display.cpp
@@ -11,32 +11,39 @@
 using namespace std;
 using namespace cv;
 
+bool inited = false;
+VideoWriter video;
 
 
-
-void init(int width, int height) {
-    //video = VideoWriter ("outcpp.avi",VideoWriter::fourcc('M','J','P','G'),10, Size(width,height));
-}
-
-void display(string window_name, vector<vector<Color>> &img, int frame) {
+Mat convert(vector<vector<Color>> &img) {
     int width = img[0].size();
     int height = img.size();
 
-    Mat img_mat (height, width, CV_8UC3);
+    Mat m (height, width, CV_8UC3);
 
     for (int y = 0; (long unsigned int)y < height; y++){
         for (int x = 0; (long unsigned int)x < width; x++){
-            img_mat.at<Vec3b>(Point(x,(height-1)-y)) = {img[y][x].b,img[y][x].g,img[y][x].r};
+            m.at<Vec3b>(Point(x,(height-1)-y)) = {img[y][x].b,img[y][x].g,img[y][x].r};
         }
     }
 
-    //video.write(img_mat);
-    namedWindow(window_name, WINDOW_AUTOSIZE);
-    imshow(window_name, img_mat);
-    if (waitKey(1)==0) exit(0);
+    return m; 
+}
 
-    //if(frame > 120) {
-    //    video.release();
-    //    exit(0);
-    //}
+void display(string window_name, vector<vector<Color>> &img, int frame) {
+    namedWindow(window_name, WINDOW_AUTOSIZE);
+    imshow(window_name, convert(img));
+    if (waitKey(1)==0) exit(0);
+}
+
+void fileout(string filename, vector<vector<Color>> &img, int frame, int frame_c, int fps) {
+    if (!inited) {
+        video = VideoWriter (filename,VideoWriter::fourcc('M','J','P','G'),fps, Size(img[0].size(),img.size()));
+        inited = true;
+    }
+    video.write(convert(img));
+    if(frame > frame_c) {
+        video.release();
+        exit(0);
+    }
 }
--- src/output/display.hpp
+++ src/output/display.hpp
@@ -5,3 +5,4 @@
 
 using namespace std;
 void display(string window_name, vector<vector<Color>> &img, int frame);
+void fileout(string filename, vector<vector<Color>> &img, int frame, int frame_c, int fps);
