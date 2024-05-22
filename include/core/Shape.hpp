fb18d17b1bcfd2f10d22acf587e5d5aba3eca936
Marcin Mennemann <marcinmennemann@gmail.com>
2024-05-22 15:38:07 +0200

    added transparency to Shape.hpp

--- include/core/Shape.hpp
+++ include/core/Shape.hpp
@@ -12,6 +12,7 @@ class Shape {
 
     Color col;
     double refl;
+    double tran;
 
     virtual Color color(Vec3 postition) {
         return this->col;
@@ -21,7 +22,11 @@ class Shape {
         return this->refl;
     }
 
-    Shape(Vec3 pos, Color color, double reflectance = 0) : position(pos), col(color), refl(reflectance) {}
+    virtual double transparency(Vec3 position) {
+        return this->tran;
+    }
+
+    Shape(Vec3 pos, Color color, double reflectance = 0, double transparency = 0) : position(pos), col(color), refl(reflectance), tran(transparency) {}
 
     virtual double sdf(Vec3 position) {
         return std::numeric_limits<double>::max();
