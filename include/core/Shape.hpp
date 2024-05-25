fa160faa1ab449284f45d1827494b7d75fb45526
Marcin Mennemann <marcinmennemann@gmail.com>
2024-05-26 01:22:39 +0200

    fixed shape function args

--- include/core/Shape.hpp
+++ include/core/Shape.hpp
@@ -15,22 +15,22 @@ class Shape {
     double refl;
     double tran;
 
-    virtual std::string color(Vec3 postition) {
+    virtual std::string color() {
         return to_glsl_vec3({col.r,col.g,col.b});
     }
 
-    virtual std::string reflectance(Vec3 position) {
+    virtual std::string reflectance() {
         return std::to_string(refl);
     }
 
-    virtual std::string transparency(Vec3 position) {
+    virtual std::string transparency() {
         return std::to_string(tran);
     }
 
     Shape(Vec3 pos, Color color, double reflectance = 0, double transparency = 0) : position(pos), col(color), refl(reflectance), tran(transparency) {}
 
-    virtual std::string sdf(Vec3 position) {
-        return "";
+    virtual std::string sdf() {
+        return "0";
     }
 };
 
